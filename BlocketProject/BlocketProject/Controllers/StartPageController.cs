26b0e79565023780084ede455b4f504eb0a389e2
aelex91 <alexanderhelsinghof@hotmail.com>
2014-08-19 09:38:26 +0200

    Wip
    WiLoneles

---
+++ BlocketProject/BlocketProject/Controllers/StartPageController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using EPiServer;
+using EPiServer.Core;
+using EPiServer.Framework.DataAnnotations;
+using EPiServer.Web.Mvc;
+using BlocketProject.Models.Pages;
+
+namespace BlocketProject.Controllers
+{
+    public class StartPageController : PageController<StartPage>
+    {
+        public ActionResult Index(StartPage currentPage)
+        {
+            var mask = "Hej";
+            /* Implementation of action. You can create your own view model class that you pass to the view or
+             * you can pass the page type for simpler templates */
+
+            return View(currentPage);
+        }
+    }
+}
---
+++ BlocketProject/BlocketProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BlocketProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BlocketProject")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d1398db1-68c7-44c2-9231-ac5d88d2b7f5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
