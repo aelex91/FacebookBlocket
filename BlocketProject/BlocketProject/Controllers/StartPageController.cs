3a85407cffe57f2bc5a145625c8e975884d4aeb4
Ludvig Flemmich <ludde_flemmich@hotmail.com>
2014-08-25 16:51:33 +0200

    Added Facebook properties to appSettings, added methods for authenticating facebook login

--- BlocketProject/BlocketProject/Controllers/StartPageController.cs
+++ BlocketProject/BlocketProject/Controllers/StartPageController.cs
@@ -6,21 +6,116 @@ using EPiServer.Core;
 using EPiServer.Framework.DataAnnotations;
 using EPiServer.Web.Mvc;
 using BlocketProject.Models.Pages;
+using System.Configuration;
+using System.Web.Configuration;
+using System.Net;
+using System.IO;
+using Facebook;
+using System.Web;
+using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
 
 namespace BlocketProject.Controllers
 {
     public class StartPageController : PageController<StartPage>
     {
+        string appId = WebConfigurationManager.AppSettings["FacebookAppID"];
+        string appSecret = WebConfigurationManager.AppSettings["FacebookAppSecret"];
+        string scope = "user_likes, publish_stream, manage_pages, user_friends, email";
+        string redirectUrl = "http://letemsale.com/";
+
         public ActionResult Index(StartPage currentPage)
         {
-          
+
+            if (Request["code"]!= null)
+            {
+                CheckAuthorization();
+            }
+            
             return View(currentPage);
         }
+
         [HttpPost]
-        public ActionResult Hejsan()
+        public ActionResult Authenticate()
         {
-            Response.Write("hej");
-            return View("Index");
+
+            CheckAuthorization();
+            return RedirectToAction("Index");
+
+            /*
+             * Klicka på knappen
+    
+             * Redirect till Facebook för autentisering/inloggning
+             * Hämta facebookID och tillgängliga användaruppgifter
+             * Lagra användare och redirect till användarvy.
+             */
+        }
+
+        public void CheckAuthorization()
+        {
+
+            if (Request["code"] == null)
+            {
+
+                Response.Redirect(string.Format("https://graph.facebook.com/oauth/authorize?client_id={0}&redirect_uri={1}&scope={2}", appId, redirectUrl, scope));
+
+            }
+
+            else
+            {
+
+                Dictionary<string, string> tokens = new Dictionary<string, string>();
+
+
+
+                string url = string.Format("https://graph.facebook.com/oauth/access_token?client_id={0}&redirect_uri={1}&scope={2}&code={3}&client_secret={4}",
+
+                    appId, Request.Url.AbsoluteUri, scope, Request["code"].ToString(), appSecret);
+
+
+
+                HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+
+
+
+                using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+                {
+
+                    StreamReader reader = new StreamReader(response.GetResponseStream());
+
+
+
+                    string vals = reader.ReadToEnd();
+
+
+
+                    foreach (string token in vals.Split('&'))
+                    {
+
+                        
+
+                        tokens.Add(token.Substring(0, token.IndexOf("=")), token.Substring(token.IndexOf("=") + 1, token.Length - token.IndexOf("=") - 1));
+
+                    }
+
+                }
+
+                string access_token = tokens["access_token"];
+                var client = new FacebookClient(access_token);
+
+                JsonObject user = client.Get("me/") as JsonObject;
+
+                JObject json = JObject.Parse(user.ToString());
+                
+                
+
+               
+
+            }
+
+           
         }
     }
 }
