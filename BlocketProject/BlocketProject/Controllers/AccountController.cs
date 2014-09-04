3236b6df7db9cc59b8629140b7085f84295572a4
unknown <xxludvigfl@HIQSTOW158.sto.hiq.se>
2014-09-04 15:49:52 +0200

    AccountController for signing out

---
+++ BlocketProject/BlocketProject/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using BlocketProject.Helpers;
+using EPiServer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace BlocketProject.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Account/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public void Logout()
+        {
+            FormsAuthentication.SignOut();
+            Session.Clear();
+            UrlHelper url = new UrlHelper(System.Web.HttpContext.Current.Request.RequestContext);
+            Response.Redirect(UrlHelpers.PageLinkUrl(url, PageReference.StartPage).ToHtmlString());
+
+        }
+
+    }
+}
