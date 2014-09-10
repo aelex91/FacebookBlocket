5c0bea17cce00405af68c4f177cb0d45cf2ebdc0
unknown <xxludvigfl@HIQSTOW158.sto.hiq.se>
2014-09-10 08:41:25 +0200

    Shame on me

---
+++ BlocketProject/BlocketProject/Models/ViewModels/FacebookFriendsModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlocketProject.Models.ViewModels
+{
+    public class FacebookFriendsModel
+    {
+        public List<FacebookFriend> friendsList { get; set; }
+    }
+
+    public class FacebookFriend
+    {
+        public string name { get; set; }
+        public string id { get; set; }
+    }
+}
