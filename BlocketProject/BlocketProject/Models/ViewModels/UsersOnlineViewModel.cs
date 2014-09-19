1449220312d3dfc9b85f691a22e9019f5bb1da0d
Alexander Helsinghof <alexander.helsinghof@hiq.se>
2014-09-19 13:43:50 +0200

    useronlineviewmodel

---
+++ BlocketProject/BlocketProject/Models/ViewModels/UsersOnlineViewModel.cs
@@ -0,0 +1,19 @@
+﻿using BlocketProject.Models.Blocks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlocketProject.Models.ViewModels
+{
+    public class UsersOnlineViewModel
+    {
+        public UsersOnlineViewModel(UsersOnlineBlock currentBlock)
+        {
+
+        }
+        public int RegistredUsers { get; set; }
+        public int MaleUsers { get; set; }
+        public int FemaleUsers { get; set; }
+    }
+}
