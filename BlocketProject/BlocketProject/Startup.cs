72b2cb6062ecdc0d6de1b85f42cb9c0e54fe6ffd
Alexander Helsinghof <alexander.helsinghof@hiq.se>
2014-09-23 08:35:07 +0200

    startup signalR cs

---
+++ BlocketProject/BlocketProject/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using Microsoft.Owin;
+using Owin;
+[assembly: OwinStartup(typeof(BlocketProject.Startup))]
+
+namespace BlocketProject
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+        }
+
+    }
+}
