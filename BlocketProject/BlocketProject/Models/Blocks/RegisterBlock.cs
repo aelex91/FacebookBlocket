a1f911400f92b916ef71a068eaea2882610a78e1
Alexander Helsinghof <alexander.helsinghof@hiq.se>
2014-09-24 08:40:11 +0200

    registerblock

---
+++ BlocketProject/BlocketProject/Models/Blocks/RegisterBlock.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+
+namespace BlocketProject.Models.Blocks
+{
+    [ContentType(DisplayName = "RegisterBlock", GUID = "4b8d5626-df0e-4ee7-90ff-742735da4030", Description = "")]
+    public class RegisterBlock : BlockData
+    {
+
+        [CultureSpecific]
+        [Display(
+            Name = "Text Label",
+            Description = "Text över inloggingen, t.ex: 'Joina hemsidanamn och hitta ditt event ikväll'!",
+            GroupName = SystemTabNames.Content,
+            Order = 100)]
+        public virtual string Text { get; set; }
+
+        [CultureSpecific]
+        [Display(
+            Name = "Name Label",
+            GroupName = SystemTabNames.Content,
+            Order = 200)]
+        public virtual string FirstNameLabel { get; set; }
+
+        [CultureSpecific]
+        [Display(
+            Name = "LastName Label",
+            GroupName = SystemTabNames.Content,
+            Order = 250)]
+        public virtual string LastNameLabel { get; set; }
+
+        [CultureSpecific]
+        [Display(
+            Name = "Birth Label",
+            GroupName = SystemTabNames.Content,
+            Order = 300)]
+        public virtual string BirthLabel { get; set; }
+
+        [CultureSpecific]
+        [Display(
+            Name = "City Label",
+            GroupName = SystemTabNames.Content,
+            Order = 400)]
+        public virtual string CityLabel { get; set; }
+
+        [CultureSpecific]
+        [Display(
+            Name = "Gender Label",
+            GroupName = SystemTabNames.Content,
+            Order = 500)]
+        public virtual string GenderLabel { get; set; }
+
+        [CultureSpecific]
+        [Display(
+            Name = "Email Label",
+            GroupName = SystemTabNames.Content,
+            Order = 500)]
+        public virtual string EmailLabel { get; set; }
+
+        [CultureSpecific]
+        [Display(
+            Name = "Register Label Sign Up",
+            GroupName = SystemTabNames.Content,
+            Order = 600)]
+        public virtual string RegisterLabel { get; set; }
+
+
+    }
+}
---
+++ BlocketProject/BlocketProject/Models/ViewModels/RegisterBlockViewModel.cs
@@ -0,0 +1,53 @@
+﻿using BlocketProject.Models.Blocks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlocketProject.Models.ViewModels
+{
+    public class RegisterBlockViewModel
+    {
+        public RegisterBlockViewModel(RegisterBlock currentBlock)
+        {
+            this.FirstNameLabel = currentBlock.FirstNameLabel;
+            this.LastNameLabel = currentBlock.LastNameLabel;
+            this.EmailLabel = currentBlock.EmailLabel;
+            this.CityLabel = currentBlock.CityLabel;
+            this.GenderLabel = currentBlock.GenderLabel;
+            this.Text = currentBlock.Text;
+        }
+        public string FirstNameLabel { get; set; }
+        public string LastNameLabel { get; set; }
+        public string BirthLabel { get; set; }
+        public string CityLabel { get; set; }
+        public string GenderLabel { get; set; }
+        public string EmailLabel { get; set; }
+        public string Text { get; set; }
+        public Register RegisterUser { get; set; }
+
+    }
+
+    public class Register
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+
+        public string SelectedGender { get; set; }
+        public string SelectedCity { get; set; }
+
+        public string SelectedDay { get; set; }
+        public string SelectedMonth { get; set; }
+        public string SelectedYear { get; set; }
+
+        public Dictionary<int, string> City { get; set; }
+        public Dictionary<int, string> Gender { get; set; }
+        public Dictionary<int, int> Day { get; set; }
+        public Dictionary<int, int> Month { get; set; }
+        public Dictionary<int, int> Year { get; set; }
+
+    }
+}
