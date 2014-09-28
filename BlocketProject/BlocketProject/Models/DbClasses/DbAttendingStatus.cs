6b60f233562cf6773c588b1b697ce5329a614403
unknown <xxludvigfl@HIQSTOW158.sto.hiq.se>
2014-09-28 14:27:41 +0200

    no message

---
+++ BlocketProject/BlocketProject/Models/DbClasses/DbAttendingStatus.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BlocketProject.Models.DbClasses
+{
+    [Table("AttendingStatus")]
+    public class DbAttendingStatus
+    {
+        
+        [Key, Column("StatusId")]
+        public int StatusId { get; set; }
+        public string StatusName { get; set; }
+
+    }
+}
---
+++ BlocketProject/BlocketProject/Models/DbClasses/DbGuestList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+
+namespace BlocketProject.Models.DbClasses
+{
+    [Table("GuestList")]
+    public class DbGuestList
+    {
+        public int UserId { get; set; }
+        public int EventId { get; set; }
+        [Key, Column("StatusId")]
+        public int StatusId { get; set; }
+        public int InvitedByUserId { get; set; }
+    }
+}
